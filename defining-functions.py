71900f43eaa2a316982a7584cef6fdc1832088b1
PETER MWANGI <gichimupeter07@gmail.com>
2026-07-28 23:45:15 +0300

    Function blocks in Python.

---
+++ defining-functions.py
@@ -0,0 +1,7 @@
+# Defining a function block
+def show_daily_goal():
+    print("Target step goals: 8,000 steps.")
+    print("Target water intake: 8 glasses.")
+    print("Cold Shower: Yes")
+
+show_daily_goal()
