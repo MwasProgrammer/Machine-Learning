8a6a240d1e87fe620a967b67964ba94824f4409b
PETER MWANGI <gichimupeter07@gmail.com>
2026-08-08 12:41:38 +0300

    Refactored list comprehension.

--- day15-list-comprehension.py
+++ day15-list-comprehension.py
@@ -2,7 +2,7 @@ step_profile = [
     {"name" : "Peter", "steps" : [7368, 9873, 10912, 6352, 8192, 5738, 7322]},
     {"name" : "Rael", "steps" : [9368, 7873, 10012, 6252, 7192, 5748, 9322]},
     {"name" : "Hanif", "steps" : [5368, 8973, 9412, 10352, 5192, 9738, 7622]},
-    {"name" : "Jay", "steps" : [7648, 9863, 10972, 6351, 8592, 5838, 9322]},
+    {"name" : "Jay", "steps" : [72648, 9863, 10972, 6351, 8592, 5838, 9322]},
 ]
 
 step_above_10000 = [day_steps for profile in step_profile for day_steps in profile["steps"] if day_steps > 10000]
