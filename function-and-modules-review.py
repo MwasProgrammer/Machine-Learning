581894c06ad22c28a111f08cf73108d97f9b3d2c
PETER MWANGI <gichimupeter07@gmail.com>
2026-08-25 23:55:18 +0300

    Use a list comprehension to get all even numbers from 1 to 30

--- function-and-modules-review.py
+++ function-and-modules-review.py
@@ -2,3 +2,11 @@
 # Functions, modules, and packages
 # List comprehensions
 # Default parameters
+
+# Use a list comprehension to get all even numbers from 1 to 30
+evens = [n for n in range(1, 31) if n % 2 == 0]
+print(evens)
+
+# Now get the squares of those even numbers
+squares = [n**2 for n in evens]
+print(squares)
