9f0e53c3b9a3e2501f9b2157f526c8a082de108f
PETER MWANGI <gichimupeter07@gmail.com>
2026-08-31 06:57:29 +0300

    Move files into folders

--- day2.py
+++
@@ -1,9 +0,0 @@
-name = "Peter Mwangi"
-age = 24
-height = 1.84
-citizenship = "Kenyan"
-city = "Nairobi"
-occupation = "Software Engineer"
-
-#output
-print(f"My name is {name}. I am a {age} year old {citizenship} living in {city}. I work as a {occupation} and my height is {height} meters.")
